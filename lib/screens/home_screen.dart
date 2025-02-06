ee193d291e3409d07faafc4f8b62fde5129611a6
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2025-02-06 17:40:41 +0545

    feat: Add Profile Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:travel_companion_app/widgets/category_section.dart';
 import 'package:travel_companion_app/widgets/destination_card.dart';
 import 'package:travel_companion_app/widgets/location_header.dart';
@@ -8,56 +9,59 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            debugPrint('Menu button pressed');
+          },
+        ),
+        actions: const [
+          CircleAvatar(
+            radius: 20,
+            backgroundImage: AssetImage('assets/images/person.png'),
+          ),
+          SizedBox(width: 16),
+        ],
+        elevation: 0,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+      ),
+      body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            AppBar(
-              leading: IconButton(
-                icon: const Icon(Icons.menu),
-                onPressed: () {
-                  debugPrint('Menu button pressed');
-                },
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 16),
+              const LocationHeader(
+                location: 'Patan, Kathmandu',
               ),
-              actions: const [
-                CircleAvatar(
-                  radius: 20,
-                  backgroundImage: AssetImage('assets/images/person.png'),
-                ),
-                SizedBox(width: 16),
-              ],
-              elevation: 0,
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.black,
-            ),
-            const SizedBox(height: 16),
-            const LocationHeader(
-              location: 'Patan, Kathmandu',
-            ),
-            const SizedBox(height: 24),
-            const CategorySection(),
-            const SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Best Destination',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+              const SizedBox(height: 24),
+              const CategorySection(),
+              const SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Best Destination',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('View all'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            const DestinationGrid(),
-          ],
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('View all'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              const DestinationGrid(),
+            ],
+          ),
         ),
       ),
     );
